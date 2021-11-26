b28c89a67e994797304351da64a892800b22d812
morenm14 <morenm14@gmatc.matc.edu>
2021-11-26 17:47:22 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './components/Home';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React</h1>
+    
+    <Home/>
+    <Menu/>
+    </div>
+  );
+}
+
+export default App;
